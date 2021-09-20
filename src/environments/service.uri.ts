82e102d03fee560029304a69bc0c5ae374ac200b
Gorle <gorleshanmukh@gmail.com>
2021-09-20 10:49:41 +0530

    initial

---
+++ src/environments/service.uri.ts
@@ -0,0 +1,15 @@
+const SERVICE_BASE_URL = "http://52.66.230.172:8080";
+export const SERVICE_URI = {
+  loginURI : SERVICE_BASE_URL+"/login",
+  registerURI : SERVICE_BASE_URL+"/register",
+  getUser : SERVICE_BASE_URL+"/homepage",
+  forgotPassword : SERVICE_BASE_URL+"/forgotpassword",
+  getAllTweets : SERVICE_BASE_URL+"/all",
+  addTweet : SERVICE_BASE_URL+"/add",
+  likeTweet : SERVICE_BASE_URL+"/like",
+  getReplies : SERVICE_BASE_URL+"/all/reply",
+  replyTweet : SERVICE_BASE_URL+"/reply",
+  getAllUsers : SERVICE_BASE_URL+"/getAllUser",
+    tweetAgent : "http://65.0.71.255:8080",
+  uploadFile : "http://localhost:8083"
+}
